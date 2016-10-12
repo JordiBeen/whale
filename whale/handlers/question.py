8c48e00275437d72c5af20719c635ba750f9f977
jordibeen <jordibeen1996@gmail.com>
2016-10-12 16:55:36 +0200

    Basic interaction with bot

--- whale/handlers/question.py
+++ whale/handlers/question.py
@@ -12,10 +12,63 @@ def question_post(request):
     settings = request.registry.settings
     request_data = request.json_body
 
-    if request_data.get('token') != settings.get('whale.token'):
+    message = ''
+    token = ''
+    answer_to = ''
+
+    return_data = {}
+    return_data['status'] = 'error'
+    return_data['answer'] = 'Sorry, I do not understand'
+
+    log.info("{} Start of log: '{}' {}".format("-" * 40, "request_data", "-" * 40))
+    log.info(request_data)
+    log.info("{} End of log: '{}' {}".format("-" * 40, "request_data", "-" * 40))
+
+    if 'message' in request_data:
+        message = request_data.get('message')
+    else:
+        return_data['answer'] = 'Please specify a message'
+
+    if 'answer_to' in request_data:
+        answer_to = request_data.get('answer_to')
+
+    if 'token' in request_data:
+        token = request_data.get('token')
+
+    if token != settings.get('whale.token'):
         return httpexceptions.HTTPForbidden()
 
-    return_data = {"ja": "het werkt"}
+    return_data['status'] = 'OK'
+
+    # User asking to vibrate
+    if 'vibrate' in message:
+        return_data['answer'] = 'Okay, how long should I vibrate?'
+
+    # User answers to a question asked
+    if answer_to:
+        # User is answering how long to vibrate
+        if all(x in answer_to for x in ['how', 'long', 'vibrate']):
+            if string_has_number(message):
+                length = get_number_from_string(message)
+                if 'second' in message:
+                    length = length
+                if 'minute' in message:
+                    length = length * 60
+                if 'hour' in message:
+                    length = length * 60 * 60
+                return_data['answer'] = 'Going to vibrate, enjoy!'
+                return_data['function'] = {}
+                return_data['function']['name'] = 'vibrate'
+                return_data['function']['length'] = length
+
     return {
         'data': return_data
     }
+
+
+def get_number_from_string(string):
+    return [int(s) for s in string.split() if s.isdigit()][0]
+
+
+def string_has_number(string):
+    return any(char.isdigit() for char in string)
